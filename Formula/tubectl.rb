45be6682f5f5615d810404af707a6290ea8b9d0e
raffis <sahli@gyselroth.com>
2018-12-17 14:58:59 +0100

    fixes sha hash

--- Formula/tubectl.rb
+++ Formula/tubectl.rb
@@ -2,7 +2,7 @@ class Tubectl < Formula
   desc "The tubecli client for OS X"
   homepage "https://github.com/gyselroth/tubee-client-cli"
   url "https://github.com/gyselroth/tubee-client-cli/releases/download/v1.0.0-alpha14/tubectl-osx-x64"
-  sha256 "b1c7ab25dfb4530a5e35aa690d79469de5ec419dd284f03868935c2417e1ee3a"
+  sha256 "c21b7b5a9e824ab43065d7588c05c8f9c6d601712b4a762cb8ee56e036f103ac"
   version "1.0.0-alpha14"
 
   bottle :unneeded
