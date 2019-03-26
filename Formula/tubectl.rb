bbd08b363da22c37d464c771aca533fed21b4faf
raffis <sahli@gyselroth.com>
2019-03-26 13:20:28 +0100

    description

--- Formula/tubectl.rb
+++ Formula/tubectl.rb
@@ -1,5 +1,5 @@
 class Tubectl < Formula
-  desc "The tubecli client for OS X"
+  desc "The tubectl for OS X"
   homepage "https://github.com/gyselroth/tubee-client-cli"
   url "https://github.com/gyselroth/tubee-client-cli/releases/download/v1.0.0-beta3/tubectl-osx-x64"
   sha256 "adedb4efe8affb669da567138f4faa342df3d759587faa1777d90018e17bc9f5"
