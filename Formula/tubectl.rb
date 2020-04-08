f61e0f211f5628610a3be5c9179b98baf140f79e
Travis CI <travis@travis-ci.org>
2020-04-08 08:20:59 +0000

    tubectl release 1.0.0-beta19

--- Formula/tubectl.rb
+++ Formula/tubectl.rb
@@ -1,9 +1,9 @@
 class Tubectl < Formula
   desc "The tubectl for OS X"
   homepage "https://github.com/gyselroth/tubee-client-cli"
-  url "https://github.com/gyselroth/tubee-client-cli/releases/download/v1.0.0-beta18/tubectl-osx-x64"
-  sha256 "bc0a791af185617542ddee93ca21a131da974b87800fbe4f9c4ab331e9e3451f"
-  version "1.0.0-beta18"
+  url "https://github.com/gyselroth/tubee-client-cli/releases/download/v1.0.0-beta19/tubectl-osx-x64"
+  sha256 "842739a99f8ece0682127184840ec920e06ceb4dc564b958e71789f57cfeb349"
+  version "1.0.0-beta19"
 
   bottle :unneeded
 
