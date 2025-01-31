a2a476f4d36cb9ff588a9f6329373b584071a7c1
circleci Build <no-reply@gyselroth.net>
2025-01-31 11:54:16 +0000

    tubectl release v1.3.2-beta2

--- Formula/tubectltest.rb
+++ Formula/tubectltest.rb
@@ -1,16 +1,16 @@
 class Tubectltest < Formula
   desc "tubectl testing"
   homepage "https://github.com/gyselroth/tubee-client-cli"
-  version "1.3.2-beta1"
+  version "1.3.2-beta2"
 
   on_arm do
     sha256 "d89224bc9874e9c5841d6c5ea6084a28009d22ffd3e674174502023c566f749f"
-    url "https://github.com/gyselroth/tubee-client-cli/releases/download/v1.3.2-beta1/tubectl-osx-arm64"
+    url "https://github.com/gyselroth/tubee-client-cli/releases/download/v1.3.2-beta2/tubectl-osx-x64"
   end
 
   on_intel do
     sha256 "b3dd6952b57616ebd050a14982e2ce0f29171eaa8f050a60c15308a7e0898f7c"
-    url "https://github.com/gyselroth/tubee-client-cli/releases/download/v1.3.2-beta1/tubectl-osx-x64"
+    url "https://github.com/gyselroth/tubee-client-cli/releases/download/v1.3.2-beta2/tubectl-osx-x64"
   end
 
   def install
