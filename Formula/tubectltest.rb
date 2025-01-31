2462d08025832b030e4d1c0c77cbada1fd69a264
s-aebischer <aebischer@gyselroth.com>
2025-01-31 12:14:33 +0100

    formula testing

--- Formula/tubectltest.rb
+++ Formula/tubectltest.rb
@@ -4,13 +4,13 @@ class Tubectltest < Formula
   version "1.3.2-beta1"
 
   on_arm do
-    url "https://github.com/gyselroth/tubee-client-cli/releases/download/v1.3.2-beta1/tubectl-osx-arm64"
     sha256 "d89224bc9874e9c5841d6c5ea6084a28009d22ffd3e674174502023c566f749f"
+    url "https://github.com/gyselroth/tubee-client-cli/releases/download/v1.3.2-beta1/tubectl-osx-arm64"
   end
 
   on_intel do
-    url "https://github.com/gyselroth/tubee-client-cli/releases/download/v1.3.2-beta1/tubectl-osx-x64"
     sha256 "b3dd6952b57616ebd050a14982e2ce0f29171eaa8f050a60c15308a7e0898f7c"
+    url "https://github.com/gyselroth/tubee-client-cli/releases/download/v1.3.2-beta1/tubectl-osx-x64"
   end
 
   def install
