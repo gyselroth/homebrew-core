852f9651d7d16b9011b6b8dc14d47f40fcbdff40
s-aebischer <aebischer@gyselroth.com>
2025-01-31 09:59:44 +0100

    added tubectl-x64 formula

---
+++ Formula/tubectl-x64.rb
@@ -0,0 +1,12 @@
+class Tubectl-x64 < Formula
+  desc "The tubectl-x64 for OS X"
+  homepage "https://github.com/gyselroth/tubee-client-cli"
+  url "https://github.com/gyselroth/tubee-client-cli/releases/download/v1.3.2-beta1/tubectl-osx-x64"
+  sha256 "b3dd6952b57616ebd050a14982e2ce0f29171eaa8f050a60c15308a7e0898f7c"
+  version "1.3.2-beta1"
+
+  def install
+    bin.install "tubectl-osx-x64"
+    mv bin/"tubectl-osx-x64", bin/"tubectl"
+  end
+end
