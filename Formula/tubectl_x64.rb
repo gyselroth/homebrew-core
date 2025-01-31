211e92025bfdaff8f0b4794c15d6f48376f95290
s-aebischer <aebischer@gyselroth.com>
2025-01-31 10:09:18 +0100

    rename tubectl-x64

--- Formula/tubectl_x64.rb
+++ Formula/tubectl_x64.rb
@@ -1,5 +1,5 @@
 class Tubectl_x64 < Formula
-  desc "The tubectl-x64 for OS X"
+  desc "The tubectl_x64 for OS X"
   homepage "https://github.com/gyselroth/tubee-client-cli"
   url "https://github.com/gyselroth/tubee-client-cli/releases/download/v1.3.2-beta1/tubectl-osx-x64"
   sha256 "b3dd6952b57616ebd050a14982e2ce0f29171eaa8f050a60c15308a7e0898f7c"
