828cd0c5b542f9678ddb841238d3b7ed234516f6
circleci Build <no-reply@gyselroth.net>
2026-01-04 23:59:52 +0000

    tubectl release v1.3.5

--- Formula/tubectl.rb
+++ Formula/tubectl.rb
@@ -1,9 +1,9 @@
 class Tubectl < Formula
   desc "The tubectl for OS X"
   homepage "https://github.com/gyselroth/tubee-client-cli"
-  url "https://github.com/gyselroth/tubee-client-cli/releases/download/v1.3.1/tubectl-osx-x64"
-  sha256 "bbbd543a6e10d3cb02c242be1bf155ad9c85206ebbf955f6dcdc8ded0f539fcb"
-  version "1.3.1"
+  url "https://github.com/gyselroth/tubee-client-cli/releases/download/v1.3.5/tubectl-osx-x64"
+  sha256 "efe6d3db7a92d0cdd648b800298b5e26e0cd1ef954e0f725a87f79d94ac8119f"
+  version "1.3.5"
 
   def install
     bin.install "tubectl-osx-x64"
