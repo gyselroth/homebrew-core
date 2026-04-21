9cdd6a53055ad4509c07e759dbe8e806d18b4423
s-aebischer <aebischer@gyselroth.com>
2026-04-21 22:50:42 +0200

    tubectl release v1.3.6

--- Formula/tubectl.rb
+++ Formula/tubectl.rb
@@ -1,9 +1,9 @@
 class Tubectl < Formula
   desc "The tubectl for OS X"
   homepage "https://github.com/gyselroth/tubee-client-cli"
-  url "https://github.com/gyselroth/tubee-client-cli/releases/download/v1.3.5/tubectl-osx-x64"
-  sha256 "efe6d3db7a92d0cdd648b800298b5e26e0cd1ef954e0f725a87f79d94ac8119f"
-  version "1.3.5"
+  url "https://github.com/gyselroth/tubee-client-cli/releases/download/v1.3.6/tubectl-osx-x64"
+  sha256 "eb5450ee066ac0e5d18d604ea1c7eb72517fa6f205b5cc011fb801e71bb62077"
+  version "1.3.6"
 
   def install
     bin.install "tubectl-osx-x64"
